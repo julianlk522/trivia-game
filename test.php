10fdb2ce9bc14004c72cb18f158f2fce97c43fdd
Julian Lindsay-Kaufman <jjjuliannnn456@gmail.com>
2022-10-31 11:46:11 -0400

    add Dockerfile for php server to allow mysqli installation, replace default php:apache image in docker-compose with image from Dockerfile, replace test echo with basic working connection/query/markup in test.php

--- test.php
+++ test.php
@@ -1,3 +1,20 @@
 <?php
 
-echo 'Hellooooo';
+$connect = mysqli_connect(
+    'db',
+    'trivia_user',
+    'trivia_pass',
+    'lamp_trivia_game'
+);
+
+$query = 'SELECT * FROM users';
+$result = mysqli_query($connect, $query);
+
+echo '<h1>MySQL Content:</h1>';
+
+while($record = mysqli_fetch_assoc($result)) {
+    echo '<p>'.$record['user_id'].'</p>';
+    echo '<p>'.$record['username'].'</p>';
+    echo '<p>'.$record['password'].'</p>';
+    echo '<hr />';
+}
